62a08923838cfbc14a9113aa0f4bf00a29f746b9
SUGATACHANDA <scbabai2704@gmail.com>
2024-12-03 12:36:18 +0530

    First Commit

---
+++ server/models/Recipe.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'This field is required.'
+  },
+  description: {
+    type: String,
+    required: 'This field is required.'
+  },
+  email: {
+    type: String,
+    required: 'This field is required.'
+  },
+  ingredients: {
+    type: Array,
+    required: 'This field is required.'
+  },
+  category: {
+    type: String,
+    enum: ['Thai', 'American', 'Chinese', 'Mexican', 'Indian'],
+    required: 'This field is required.'
+  },
+  image: {
+    type: String,
+    required: 'This field is required.'
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+recipeSchema.index({ name: 'text', description: 'text' });
+// WildCard Indexing
+//recipeSchema.index({ "$**" : 'text' });
+
+module.exports = mongoose.model('Recipe', recipeSchema);
